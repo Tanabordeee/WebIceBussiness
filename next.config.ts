cc64ebdc400b052203875653a0206953849b39a5
Tanabordee Pateepbutsagon <102315593+Tanabordeee@users.noreply.github.com>
2024-11-11 22:55:47 +0700

    update config

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    domains: ['asiaice.co.th','i.imgur.com'],
+    domains: ['asiaice.co.th', 'i.imgur.com'],
   },
+  serverExternalPackages: ['mongoose'], 
 };
+
 export default nextConfig;
