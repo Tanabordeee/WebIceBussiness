81d8654d5742c0485cae876cf6d734d16c0bbe67
Tanabordee Pateepbutsagon <102315593+Tanabordeee@users.noreply.github.com>
2024-11-11 22:59:44 +0700

    fix connectmongodb

--- lib/mongodb.ts
+++ lib/mongodb.ts
@@ -1,16 +1,17 @@
 import mongoose from "mongoose";
 
+let isConnected = false;
+
 export async function connectMongoDB() {
-  if (mongoose.connection.readyState === 1) {
-    console.log("MongoDB is already connected");
-    return;
-  }
-  
-  try {
-    await mongoose.connect(process.env.DB_CONN_STRING as string);
-    console.log("Connected to MongoDB");
-  } catch (error) {
-    console.log("Failed to connect to MongoDB", error);
-    throw new Error("Database connection failed");
-  }
+    if (isConnected) {
+        console.log("Using existing MongoDB connection");
+        return;
+    }
+    try {
+        await mongoose.connect(process.env.DB_CONN_STRING as string);
+        isConnected = true;
+        console.log("Connected to MongoDB");
+    } catch (error) {
+        console.error("Failed to connect to MongoDB", error);
+    }
 }
