16b92ff5c5ec6526bc7dd7c5e8a2d6c9954f81f5
Tanabordee Pateepbutsagon <102315593+Tanabordeee@users.noreply.github.com>
2024-11-18 21:47:26 +0700

    change statuscode and include crud

--- app/api/routes/route.tsx
+++ app/api/routes/route.tsx
@@ -1,7 +1,12 @@
 import { connectMongoDB } from "@/lib/mongodb";
 import Customer from "@/models/CustomerModel";
 import { NextResponse } from "next/server";
-interface RequestBody{
+type RequestBody ={
+    alt_description:string;
+    url:string;
+}
+type RequestUpdate = {
+    _id:string;
     alt_description:string;
     url:string;
 }
@@ -16,7 +21,7 @@ export async function POST(req:Request){
         return NextResponse.json({
             message: "Data Received successfully",
             data: {alt_description , url}
-        },{status : 201})
+        },{status : 200})
     }catch(error){
         console.error('Error handling POST request:', error);
         return NextResponse.json({ error: 'Something went wrong' }, { status: 500 });
@@ -33,3 +38,52 @@ export async function GET() {
       return NextResponse.json({ error: 'Something went wrong' }, { status: 500 });
     }
   }
+
+export async function PUT(req:Request){
+    try{
+        const {_id ,url , alt_description}: RequestUpdate = await req.json();
+        if (!_id || !url || !alt_description) {
+            return NextResponse.json({ error: 'Please send URL and Alt' }, { status: 400 });
+        }
+        await connectMongoDB();
+        const result = await Customer.updateOne({_id} , {$set:{url , alt_description}})
+        if (result.modifiedCount === 0) {
+            return NextResponse.json(
+                { message: "No document was updated. Verify _id." },
+                { status: 404 }
+            );
+        }
+        return NextResponse.json({ message: "Update successful" } , {status: 200});
+    }catch(error : any){
+        return NextResponse.json(
+            { error: "An error occurred while updating the document" },
+            { status: 500 }
+        );
+    }
+}
+type DeleteData = {
+    _id :string
+}
+export async function DELETE(req:Request){
+    try{
+        const { searchParams } = new URL(req.url);
+        const _id = searchParams.get('_id');
+        if(!_id){
+            return NextResponse.json({ error: 'Please send ID' }, { status: 400 });
+        }
+        await connectMongoDB();
+        const result = await Customer.deleteOne({_id})
+        if(result.deletedCount === 0){
+            return NextResponse.json(
+                { message: "No document was updated. Verify _id." },
+                { status: 404 }
+            );
+        }
+        return NextResponse.json({ message: "Delete successful" } , {status: 200});
+    }catch(error : any){
+        return NextResponse.json(
+            { error: "An error occurred while updating the document" },
+            { status: 500 }
+        );
+    }
+}
