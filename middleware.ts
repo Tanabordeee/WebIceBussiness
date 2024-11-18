78c159d8a995c8dbe3d18414f106c13c6639a593
Tanabordee Pateepbutsagon <102315593+Tanabordeee@users.noreply.github.com>
2024-11-18 21:46:42 +0700

    update new middle ware

--- middleware.ts
+++ middleware.ts
@@ -2,43 +2,44 @@ import { jwtVerify, importJWK } from "jose";
 import { NextResponse } from "next/server";
 
 export async function middleware(req: any) {
-  if (req.method === "POST") {
     try {
+      const pathname = req.nextUrl.pathname;
+      const method = req.method;
       let token = req.cookies.get("token");
       
-
-      if (!token) {
-        return new NextResponse("Token is missing", { status: 401 });
+      if(pathname === "/api/route" && ["POST" , "PUT" , "DELETE"].includes(method)){
+        if (!token) {
+          return new NextResponse("Token is missing", { status: 401 });
+        }
+        const secretJWK = {
+          kty: 'oct',  
+          k: process.env.JOSE_SECRET as string
+        };
+        const secretKey = await importJWK(secretJWK, 'HS256');
+  
+  
+        const { payload } = await jwtVerify(token.value, secretKey);
+  
+  
+        const requestHeaders = new Headers(req.headers);
+        requestHeaders.set('user', JSON.stringify({ name: payload.name }));
+  
+  
+        return NextResponse.next({
+          request: {
+            headers: requestHeaders,
+          },
+        });
       }
-
-
-      const secretJWK = {
-        kty: 'oct',  
-        k: process.env.JOSE_SECRET as string
-      };
-      const secretKey = await importJWK(secretJWK, 'HS256');
-
-
-      const { payload } = await jwtVerify(token.value, secretKey);
-
-
-      const requestHeaders = new Headers(req.headers);
-      requestHeaders.set('user', JSON.stringify({ name: payload.name }));
-
-
-      return NextResponse.next({
-        request: {
-          headers: requestHeaders,
-        },
-      });
+      if(pathname === "/manage/product" && method === "GET"){
+        return NextResponse.next();
+      }
+      return new NextResponse("Method not allowed", { status: 405 });
     } catch (error: any) {
 
       return new NextResponse("Error: Invalid token", { status: 403 });
     }
-  }
-
-  return NextResponse.next();
 }
 export const config = {
-  matcher: '/api/routes',
+  matcher: ['/manage/product' , "/api/route"],
 };
