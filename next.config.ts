f5386c7e837e018ed23cf6fdd2d1d90d983f834d
Tanabordee Pateepbutsagon <102315593+Tanabordeee@users.noreply.github.com>
2024-12-29 09:08:42 +0700

    delete  aisa image domain

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    domains: ['asiaice.co.th', 'i.imgur.com'],
+    domains: ['i.imgur.com'],
   },
   serverExternalPackages: ['mongoose'],
 };
