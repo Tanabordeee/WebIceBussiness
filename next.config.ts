8cba397b4827e5f49ca8c58f87caf7b3f455dc44
Tanabordee Pateepbutsagon <102315593+Tanabordeee@users.noreply.github.com>
2025-04-15 23:09:30 +0700

    update new nextconfig images

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,18 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    domains: ['img5.pic.in.th' , 'img2.pic.in.th'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'img5.pic.in.th',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'img2.pic.in.th',
+        pathname: '/**',
+      },
+    ],
   },
   serverExternalPackages: ['mongoose'],
 };
