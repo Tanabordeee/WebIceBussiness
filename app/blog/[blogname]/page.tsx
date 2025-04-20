ec0405d8e2af7d8385dc2ee4eb0120dab9d32c80
Tanabordee Pateepbutsagon <102315593+Tanabordeee@users.noreply.github.com>
2025-04-20 17:53:08 +0700

    fix : params

--- app/blog/[blogname]/page.tsx
+++ app/blog/[blogname]/page.tsx
@@ -1,11 +1,9 @@
 import Nav from "@/app/navbar";
 import BlogRenderPage from "./blogrenderpage";
 
-export default async function BlogPage({
-  params,
-}: {
-  params: Promise<{ blogname: string }>;
-}) {
+type Params = Promise<{blogname:string}>
+
+export default async function BlogPage({params} : {params : Params}) {
   const { blogname } = await params;
   return (
     <>
