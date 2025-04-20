661c4a6de7c9f5b1cf1cccd35db8ed09b9a88b72
Tanabordee Pateepbutsagon <102315593+Tanabordeee@users.noreply.github.com>
2025-04-20 16:17:26 +0700

    update interface

--- app/blog/[blogname]/page.tsx
+++ app/blog/[blogname]/page.tsx
@@ -2,13 +2,11 @@ import Nav from "@/app/navbar";
 import BlogRenderPage from "./blogrenderpage";
 
 interface BlogPageProps {
-  params: {
-    blogname: string;
-  };
+  params: Promise<{ blogname: string }>;
 }
 
-export default function BlogPage({ params }: BlogPageProps) {
-  const { blogname } = params;
+export default async function BlogPage({ params }: BlogPageProps) {
+  const { blogname } = await params; 
   return (
     <>
       <Nav />
