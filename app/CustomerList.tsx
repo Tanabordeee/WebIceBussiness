6942beed7dc87eab91164d58533226a7f29fcf84
Tanabordee Pateepbutsagon <102315593+Tanabordeee@users.noreply.github.com>
2025-06-27 19:43:12 +0700

    feat : update loader and add animation Customer

--- app/CustomerList.tsx
+++ app/CustomerList.tsx
@@ -1,4 +1,4 @@
-'use client'
+"use client";
 import axios from "axios";
 import { useEffect, useState } from "react";
 import Image from "next/image";
@@ -18,35 +18,41 @@ export default function CustomerList() {
       if (response.data?.customer) {
         setCustomers(response.data.customer);
       }
-    } catch (err : unknown) {
+    } catch (err: unknown) {
       if (err instanceof AxiosError) {
         throw new Error(err.message);
       } else {
-        throw new Error('An unexpected error occurred');
+        throw new Error("An unexpected error occurred");
       }
     }
   }
 
   useEffect(() => {
-    getCustomers();
+      getCustomers();
   }, []);
 
   return (
-    <div className="w-[80%] flex overflow-x-auto">
+    <div className="w-full">
       {customers.length > 0 ? (
-        customers.map((item, index) => (
-          <div className="min-w-[200px] h-48 flex-shrink-0 p-2" key={index}>
-            <Image
-              src={item.url}
-              alt={item.alt_description}
-              width={400} 
-              height={300}
-              className="object-cover w-full h-full"
-            />
+        <div  className="scroll-wrapper w-full">
+          <div className="scroll-track">
+            {[...customers , ...customers].map((item, index) => (
+              <div className="min-w-[200px] h-48 flex-shrink-0 p-2" key={index}>
+                <Image
+                  src={item.url}
+                  alt={item.alt_description}
+                  width={500}
+                  height={500}
+                  className="object-cover w-full h-full"
+                />
+              </div>
+            ))}
           </div>
-        ))
+        </div>
       ) : (
-        <div>DON&apos;T HAVE IMAGE</div>
+        <div className="flex items-center justify-center">
+          <div className="loader" />
+        </div>
       )}
     </div>
   );
