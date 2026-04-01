c8e374d407c4cb2c2650355cb5e658c412de0d90
tanabordeee <yok555@outlook.com>
2026-04-01 22:56:04 +0700

    fix : add put post delete in api product path

--- middleware.ts
+++ middleware.ts
@@ -2,52 +2,52 @@ import { jwtVerify, importJWK } from "jose";
 import { NextResponse } from "next/server";
 
 export async function middleware(req: any) {
-    try {
-      const pathname = req.nextUrl.pathname;
-      const method = req.method;
-      let token = req.cookies.get("token");
-      
-      if (pathname === "/api/product" && method === "GET") {
-        return NextResponse.next();
-      }
-      if (pathname === "/api/blog" && method === "GET"){
-        return NextResponse.next();
-      }
+  try {
+    const pathname = req.nextUrl.pathname;
+    const method = req.method;
+    let token = req.cookies.get("token");
 
-      if (
-        (pathname === "/api/blog" && ["POST", "PUT", "DELETE"].includes(method)) ||
-        (pathname === "/manage/product" && method === "GET") ||
-        (pathname === "/manage/blogcreate" && method === "GET") ||
-        (pathname === "/manage/blogs" && method === "GET") ||
-        (pathname.startsWith("/manage/blogupdate/") && method === "GET") 
-      ){
-        if (!token) {
-          return new NextResponse("Token is missing", { status: 401 });
-        }
-        const secretJWK = {
-          kty: 'oct',  
-          k: process.env.JOSE_SECRET as string
-        };
-        const secretKey = await importJWK(secretJWK, 'HS256');
-  
-        const { payload } = await jwtVerify(token.value, secretKey);
-  
-        const requestHeaders = new Headers(req.headers);
-        requestHeaders.set('user', JSON.stringify({ name: payload.name }));
-  
-        return NextResponse.next({
-          request: {
-            headers: requestHeaders,
-          },
-        });
-      }
-      
-      return new NextResponse("Method not allowed", { status: 405 });
-    } catch (error: any) {
-      return new NextResponse("Error: Invalid token", { status: 403 });
+    if ((pathname === "/api/product" && ["GET", "POST", "PUT", "DELETE"].includes(method))) {
+      return NextResponse.next();
     }
+    if (pathname === "/api/blog" && method === "GET") {
+      return NextResponse.next();
+    }
+
+    if (
+      (pathname === "/api/blog" && ["POST", "PUT", "DELETE"].includes(method)) ||
+      (pathname === "/manage/product" && method === "GET") ||
+      (pathname === "/manage/blogcreate" && method === "GET") ||
+      (pathname === "/manage/blogs" && method === "GET") ||
+      (pathname.startsWith("/manage/blogupdate/") && method === "GET")
+    ) {
+      if (!token) {
+        return new NextResponse("Token is missing", { status: 401 });
+      }
+      const secretJWK = {
+        kty: 'oct',
+        k: process.env.JOSE_SECRET as string
+      };
+      const secretKey = await importJWK(secretJWK, 'HS256');
+
+      const { payload } = await jwtVerify(token.value, secretKey);
+
+      const requestHeaders = new Headers(req.headers);
+      requestHeaders.set('user', JSON.stringify({ name: payload.name }));
+
+      return NextResponse.next({
+        request: {
+          headers: requestHeaders,
+        },
+      });
+    }
+
+    return new NextResponse("Method not allowed", { status: 405 });
+  } catch (error: any) {
+    return new NextResponse("Error: Invalid token", { status: 403 });
+  }
 }
 
 export const config = {
-  matcher: ['/manage/product' , "/api/blog" , "/manage/blogcreate" , "/manage/blogs" , "/manage/blogupdate" , "/api/product"],
+  matcher: ['/manage/product', "/api/blog", "/manage/blogcreate", "/manage/blogs", "/manage/blogupdate", "/api/product"],
 };
